a32671467fcbfdd70fef438d5ef187f171a09477
sagomezmartinez <sagomezmartinez@USQROSAGOMEZMA1.us.deloitte.com>
2023-09-12 08:45:36 -0500

    Testing JUnit5 Mockito - First Commit

---
+++ src/test/java/guru/springframework/InlineMockTest.java
@@ -0,0 +1,20 @@
+package guru.springframework;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class InlineMockTest {
+	
+	@Test
+	void testInlineMock() {
+		Map mapMock = mock(Map.class);
+		
+		assertEquals(mapMock.size(),0);
+	}
+	
+	
+}
